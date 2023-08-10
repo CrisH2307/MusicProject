2ec0cf7cbce311065dfa64c2e519b641f59fe027
Crisnineball <119613966+CrisH2307@users.noreply.github.com>
2023-08-10 17:11:59 -0400

    Add files via upload

---
+++ js/artists.js
@@ -0,0 +1,78 @@
+/* ********* 
+
+  artists.js
+
+  The app's list of Artists.  Each artist will be represented as an object, for example:
+
+  { 
+    id: "A1",
+    name: "The Cure",
+    links: [{url: "https://www.thecure.com", name: "Official Website" }, {url: "https://en.wikipedia.org/wiki/The_Cure" , name: "Wikipedia" }]
+  }
+
+ ********* */
+
+window.artists = [
+  {
+    id: "A1",
+    name: "The Weeknd",
+    links: [
+      {
+        url: "https://www.instagram.com/theweeknd/",
+        name: "Official Instagram",
+      },
+      { url: "https://en.wikipedia.org/wiki/The_Weeknd", name: "Wikipedia" },
+    ],
+  },
+
+  {
+    id: "A2",
+    name: "XXXTentacion",
+    links: [
+      {
+        url: "https://www.instagram.com/xxxtentacion/",
+        name: "Official Instagram",
+      },
+      { url: "https://en.wikipedia.org/wiki/XXXTentacion", name: "Wikipedia" },
+    ],
+  },
+
+  {
+    id: "A3",
+    name: "Post Malone",
+    links: [
+      {
+        url: "https://www.instagram.com/postmalone/",
+        name: "Official Instagram",
+      },
+      { url: "https://en.wikipedia.org/wiki/Post_Malone", name: "Wikipedia" },
+    ],
+  },
+
+  {
+    id: "A4",
+    name: "Drake",
+    links: [
+      {
+        url: "https://www.instagram.com/champagnepapi/",
+        name: "Official Instagram",
+      },
+      {
+        url: "https://en.wikipedia.org/wiki/Drake_(musician)",
+        name: "Wikipedia",
+      },
+    ],
+  },
+
+  {
+    id: "A5",
+    name: "Bad Bunny",
+    links: [
+      {
+        url: "https://www.instagram.com/badbunnypr/",
+        name: "Official Instagram",
+      },
+      { url: "https://en.wikipedia.org/wiki/Bad_Bunny", name: "Wikipedia" },
+    ],
+  },
+];
